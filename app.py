2a7688d8968b877cb1a54cb46b7cd677ce5e5b39
Valdir Torres Borges <valdir@localhost.localdomain>
2023-11-05 22:21:26 -0300

    ajuste docstrings

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+Pequena aplicação em Flask para demonstrar uso de pipelines
+"""
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,43 +13,57 @@ db = SQLAlchemy(app)
 
 app.app_context().push()
 
+
 class Todo(db.Model):
+    """
+    Classe Todo
+    """
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-
 @app.route("/edit")
 def home1():
+    """
+    Api edit Todo
+    """
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
 
-
 @app.route("/")
 def list1():
+    """
+    Api list Todo
+    """
     todo_list = Todo.query.all()
     return render_template("list.html", todo_list=todo_list)
 
-
 @app.route("/add", methods=["POST"])
 def add():
+    """
+    Api add Todo
+    """
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("home1"))
 
-
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
+    """
+    Api update Todo
+    """
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home1"))
 
-
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
+    """
+    Api delete Todo
+    """
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
@@ -56,4 +73,3 @@ def delete(todo_id):
 if __name__ == "__main__":
     db.create_all()
     app.run(host="0.0.0.0", debug=True)
-    
